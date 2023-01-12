37213bd7109b762a4fd2a7904433ff5d1dc0f32a
Grzegorz Gąbka <grzegorz.gabka94@gmail.com>
2023-01-12 17:42:06 +0100

    utworzenie nowego projektu

---
+++ src/main/java/com/example/AplikacjaWebowaOrganizerRolnika/AplikacjaWebowaOrganizerRolnikaApplication.java
@@ -0,0 +1,13 @@
+package com.example.AplikacjaWebowaOrganizerRolnika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplikacjaWebowaOrganizerRolnikaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplikacjaWebowaOrganizerRolnikaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AplikacjaWebowaOrganizerRolnika/AplikacjaWebowaOrganizerRolnikaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AplikacjaWebowaOrganizerRolnika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplikacjaWebowaOrganizerRolnikaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
